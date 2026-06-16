25c93ee161f4807344429b93bc575436e50dd7d7
Rydam <rid.18chauhan@gmail.com>
2026-06-16 23:03:33 +0700

    day 2: fetch PR diffs via GitHub API

--- scripts/fetch_pr_diff.py
+++ scripts/fetch_pr_diff.py
@@ -1,7 +1,9 @@
 import os 
 import json 
+import requests
 
 value= os.getenv("GITHUB_REPOSITORY")
+token= os.getenv("GITHUB_TOKEN")
 
 owner, repo = value.split("/")
 
@@ -11,4 +13,32 @@ with open(event_path) as f:
     event = json.load(f)
 
 pr_number=event["pull_request"]["number"]
+
+url=f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files"
+
+headers={
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+files= response.json()
+
+
+for file in files:
+    filename= file["filename"]
+    if (filename.endswith(".lock") or (".generated" in filename) or filename=="package-lock.json" ):
+        print(f"Skipping {filename}")
+        continue
+    patch=file.get("patch")
+    if not patch:
+        print(f"No patch available for {filename}")
+        continue
+    if patch:
+        print(f"\n=== {filename} ===")
+        print(patch)
+
     
+    
+ 
